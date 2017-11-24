9f9af6f5a5269784dc47ee34642957c41746bc6b
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-11-23 21:16:04 -0500

    first step is working refactor not working well

--- challenges/3-Dices/app.js
+++ challenges/3-Dices/app.js
@@ -10,35 +10,63 @@
 
 
 $(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
-  // Event Listener
-  $('#roller button.add').on('click', new_dice)
+	controller.load()
 
-  // Event listener
-  $('#roller button.roll').on('click', roll_dice)
+	function Model(){
+	this.random = function(){
+	return Math.floor((Math.random()*6)+1) // Model
+}
+}
+
+var model = new Model
+
+function View(){
+	 //views
+ 	this.paint_new_dice = function(){
+ 		$('.dice').append('<div class="die">0</div>') // Vista
+ 	}
+    this.paint = function(dieHtml,value){
+	$(dieHtml).text(random(value)) // Vista
+}
+}
+
+var view = new View
+
+function Controller(view, model){
+
+	this.view = view
+	this.model = model
+	  // Event Listener
+  	this.load = function(){
+  	$('#roller button.add').on('click', controller.new_dice)
+
+  	// Event listener
+  	$('#roller button.roll').on('click', controller.roll_dice)
+  	}
+	//Controller
+	this.new_dice = function() { // Controlador
+    view.paint_new_dice()
+ 	}
+    this.roll_dice = function(){
+ 		$('.die').each(function(index, dieHtml) { // Controlador
+       	view.paint(dieHtml,model.random())
+    	})
+ 	}
+}
+
+var controller = new Controller(view,model)
 })
 
 
-//Controller
-var new_dice = function() { // Controlador
-    paint_new_dice()
- }
 
- //views
- var paint_new_dice = function(){
- 	$('.dice').append('<div class="die">0</div>') // Vista
- }
 
- var roll_dice = function(){
- 	$('.die').each(function(index, dieHtml) { // Controlador
-  
-       paint(dieHtml,random())
-    })
- }
 
-var random = function(){
-	return Math.floor((Math.random()*6)+1) // Model
-}
 
-var paint = function(dieHtml,value){
-	$(dieHtml).text(random(value)) // Vista
-}
+
+
+
+
+
+
+
+
